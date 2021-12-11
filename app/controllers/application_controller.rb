1dd97739b090871f99efee11f24bd3109b14e613
ojnoh <jusinyou123@gmail.com>
2021-12-11 10:06:26 +0000

    トップページの作成とエラーの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,11 @@
 class ApplicationController < ActionController::Base
-  
-  #devise利用の機能が使われる前にconfigure_permitted_parametersが実行される
+   #devise利用の機能が使われる場合、その前にconfigure_permitted_parametersが実行されます。
    before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
-　
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
-    
+
 end
