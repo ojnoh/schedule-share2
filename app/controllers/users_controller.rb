dc59d5c0e6f534ed4d20e22c524f2d47b748fa88
ojnoh <jusinyou123@gmail.com>
2021-12-18 09:22:21 +0000

    fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @events = Event.where(user_id: @user.id)
+    @event = Event.new
   end
 
   def edit
