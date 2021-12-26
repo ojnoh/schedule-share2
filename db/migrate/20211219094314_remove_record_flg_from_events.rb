73e0f4e23214940fc90734f6ec0f8ffaa923e15a
ojnoh <jusinyou123@gmail.com>
2021-12-26 06:03:12 +0000

    fix

--- db/migrate/20211219094314_remove_record_flg_from_events.rb
+++ db/migrate/20211219094314_remove_record_flg_from_events.rb
@@ -1,6 +1,5 @@
 class RemoveRecordFlgFromEvents < ActiveRecord::Migration[5.2]
   def change
     remove_column :events, :record_flg, :boolean
-    remove_column :events, :d_flg, :boolean
   end
 end
